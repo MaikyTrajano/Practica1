ad025b4fd7bb480777bc4d90ce6d7688efe36990
MaikyTrajano <maikylvp@gmail.com>
2021-12-22 13:35:10 +0100

    primer archivo

---
+++ src/gitRepo/TestClass.java
@@ -0,0 +1,13 @@
+package gitRepo;
+
+public class TestClass {
+	private final String message;
+
+	public TestClass() {
+		this.message = "Hola mundo";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
